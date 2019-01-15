2c92f1d22b037d339fca7ce27c47d1d21440cff2
comunnidadoscar@gmail.com <comunidadoscar@gmail.com>
2019-01-15 17:55:20 -0500

    finished basic functionality

--- js/script.js
+++ js/script.js
@@ -1,21 +1,56 @@
 /* global $ */
 
 $(document).ready(function() {
-    $("button").click(function() {
-        var name = $("#name").val();
-        var techPlacement = "TBD";
-        var imgSrc = "https://media.giphy.com/media/22zgHX8aop488/giphy.gif";
-        var q1Result = $("#question1").val();
-        var q2Result = $("#question2").val();
-        var q3Result = $("#question3").val();
-        var totalScore;
+
+    let startingScore = 0;
+
+    $(".iphone").click(function(){
+        startingScore = startingScore + 20;
+        console.log(startingScore);
+    });
+    
+    $(".oneplus").click(function(){
+        startingScore = startingScore + 1;
+        console.log(startingScore);
+    });
+    $(".galaxy").click(function(){
+        startingScore = startingScore + 10;
+        console.log(startingScore);
+    });
+    $(".pixle").click(function(){
+        startingScore = startingScore + 5; 
+        console.log(startingScore);
+    });
+    // $("#one,button").click(function(){
+    //     $("#one").hide();
+    //     $("#one").attr("src","https://i.gifer.com/7efs.gif")
+    // });
+    function decision(){
+        if(startingScore < 20){
+            return "the oneplus"
+        }
+        else if(startingScore < 40){
+            return "the google pixle phone"
+        }
+        else if(startingScore <60){
+            return "a samsung galaxy phone"
+        }
+        else{
+            return "an Iphone"
+        }
+    }
+
+
+
+
+
+    $("#final").click(function() {
+        let name = $("#name").val();
+        const final = decision();
+        $("#final").html("Hey "+name+" your dream phone is "+final);
+        console.log(startingScore);
+        
+        
     });
 
 });
-//asthetic
-$("button").mouseenter(function(){
-    $("button").css("border-color", "yellow");
-  });
-  $("button").mouseleave(function(){
-    $("button").css("border-color", "white");
-  });
